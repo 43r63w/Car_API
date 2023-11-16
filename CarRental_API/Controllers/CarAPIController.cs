d3916b4eb9a59bc4598d9736afaad5df8d3f0e9e
Владислав Ткаченко <Tkachenko.V.A@nmu.one>
2023-11-16 13:22:33 +0200

    Додав перший API контролер і в ньому ж створив об'єкти моделі "Car"

---
+++ CarRental_API/Controllers/CarAPIController.cs
@@ -0,0 +1,23 @@
+﻿using CarRental_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CarRental_API.Controllers
+{
+    [Route("api/CarAPI")]
+    [ApiController]
+    public class CarAPIController : ControllerBase
+    {
+        public IEnumerable<Car> GetCars()
+        {
+            return new List<Car>
+            {
+                new Car { Id = 1,Name="BMW"},
+                new Car { Id = 2,Name="Audi"}
+            };
+
+        }
+
+
+    }
+}
